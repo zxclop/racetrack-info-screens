e62b26225e929b3ca386a9d16cbf0b41e506a301
zxclop <svyatoslav250706@gmail.com>
2026-03-23 20:03:34 +0200

    Merge branch 'main' of https://github.com/zxclop/racetrack-info-screens

--- apps/server/src/modules/race-control/race.gateway.ts
+++ apps/server/src/modules/race-control/race.gateway.ts
@@ -1,27 +1,27 @@
-// import type { Server } from "socket.io"
-// import { raceControlService } from "./race-control.service"
+import type { Server } from "socket.io"
+import { raceControlService } from "./race-control.service"
 
-// export const registerRaceGateway = (io: Server) => {
-//   io.on("connection", (socket) => {
-//     socket.emit("race:state", raceControlService.getState())
+export const registerRaceGateway = (io: Server) => {
+  io.on("connection", (socket) => {
+    socket.emit("race:state", raceControlService.getState())
 
-//     socket.on("race:start", () => {
-//       const result = raceControlService.startRace()
-//       io.emit("race:updated", result)
-//     })
+    socket.on("race:start", () => {
+      const result = raceControlService.startRace()
+      io.emit("race:updated", result)
+    })
 
-//     socket.on("race:finish", () => {
-//       const result = raceControlService.finishRace()
-//       io.emit("race:updated", result)
-//     })
+    socket.on("race:finish", () => {
+      const result = raceControlService.finishRace()
+      io.emit("race:updated", result)
+    })
 
-//     socket.on("race:end-session", () => {
-//       const result = raceControlService.endSession()
-//       io.emit("race:updated", result)
-//     })
+    socket.on("race:end-session", () => {
+      const result = raceControlService.endSession()
+      io.emit("race:updated", result)
+    })
 
-//     socket.on("disconnect", () => {
+    socket.on("disconnect", () => {
       
-//     })
-//   })
-// }
+    })
+  })
+}
